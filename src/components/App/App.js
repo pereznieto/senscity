ef21c339ba4c42a479636ca3d98cddcf92c8af8b
Fernando Macias <fernandom@motabilityoperations.co.uk>
2019-06-06 14:18:57 +0100

    Change package structure, add SCSS and initial stuff

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+const App = () => {
+  return (
+    <div className={styles.app}>
+      <h1 className={styles.title}>Senscity</h1>
+    </div>
+  );
+}
+
+export default App;
