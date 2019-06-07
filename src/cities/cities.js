335721ebc1e5b4c66a12cd272ddab889ffad9dfb
Fernando Macias <fernandom@motabilityoperations.co.uk>
2019-06-07 14:56:25 +0100

    Generate city IDs programatically

--- src/cities/cities.js
+++ src/cities/cities.js
@@ -1,10 +1,9 @@
-export const cities = [
-  { id: 0, name: 'Tokyo', latitude: 35.6850, longitude: 139.7514 },
-  { id: 1, name: 'New York', latitude: 40.6943, longitude: -73.9249 },
-  { id: 2, name: 'Mexico City', latitude: 19.4424, longitude: -99.1310 },
-  { id: 3, name: 'Mumbai', latitude: 19.0170, longitude: 72.8570 },
-  { id: 4, name: 'Sao Paulo', latitude: -23.5587, longitude: -46.6250 },
-  // { id: 5, name: 'Callibrator 0', latitude: 0, longitude: 0 },
-  // { id: 6, name: 'Callibrator 1 (80, -160)', latitude: 80, longitude: -160 },
-  // { id: 7, name: 'Callibrator 2 (-50, -160)', latitude: -50, longitude: -160 },
+const rawCities = [
+  { name: 'Tokyo', latitude: 35.6850, longitude: 139.7514 },
+  { name: 'New York', latitude: 40.6943, longitude: -73.9249 },
+  { name: 'Mexico City', latitude: 19.4424, longitude: -99.1310 },
+  { name: 'Mumbai', latitude: 19.0170, longitude: 72.8570 },
+  { name: 'Sao Paulo', latitude: -23.5587, longitude: -46.6250 },
 ];
+
+export const cities = rawCities.map((city, id) => ({ ...city, id }));
