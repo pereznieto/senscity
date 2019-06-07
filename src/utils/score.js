8d50925e4085e399ca22caeee7ea37529d87e8ca
Felix Hai Xia <fhai@thoughtworks.com>
2019-06-07 20:55:17 +0100

    Make accuracy have more weighting in calculating score

--- src/utils/score.js
+++ src/utils/score.js
@@ -3,6 +3,9 @@ import { millisecondsPerTurn } from "../components/Timer/Timer";
 const maxScorePerRound = 100;
 const halfEarthCircumference = 40075 / 2;
 
+const accuracyWeighting = 0.8;
+const timeWeighting = 0.2;
+
 const normalizeScore = score => Math.pow(score * Math.pow(maxScorePerRound, (1 / Math.E)), Math.E);
 
 export const calculatePrevScore = (distance, timeLeft) => {
@@ -10,5 +13,5 @@ export const calculatePrevScore = (distance, timeLeft) => {
   const distanceScore = normalizeScore(rawDistanceScore);
   const rawTimeScore = timeLeft / millisecondsPerTurn;
   const timeScore = normalizeScore(rawTimeScore);
-  return (distanceScore + timeScore) / 2;
+  return Math.round(distanceScore * accuracyWeighting + timeScore * timeWeighting);
 };
