6337d8a4eb30fa87a437958a543c5f96ebf079fb
Fernando Macias <fernandom@motabilityoperations.co.uk>
2019-06-26 19:58:02 +0100

    Refactor SaveScore to use state hooks

--- src/components/SaveScore/SaveScore.jsx
+++ src/components/SaveScore/SaveScore.jsx
@@ -1,49 +1,42 @@
-import { TextField, Button } from '@material-ui/core';
+import { Button, TextField } from '@material-ui/core';
 import _ from 'lodash';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './SaveScore.module.scss';
 
-class SaveScore extends React.Component {
-  constructor(props) {
-    super(props);
-    this.saveScore = this.saveScore.bind(this);
-  }
-
-  saveScore(event) {
+const SaveScore = ({ saveScore, isScoreSaved }) => {
+  const [name, setName] = useState('');
+  const saveScoreToLocalStorage = event => {
     event.preventDefault();
-    this.props.saveScore(this.state.name);
-  }
+    saveScore(name);
+  };
 
-  render() {
-    const { isScoreSaved } = this.props;
-    return (
-      <div className={styles.saveScore}>
-        <div className={styles.nameWrapper}>
-          <p className={styles.saveText}>Enter your name to save your score:</p>
-          <TextField
-            fullWidth
-            id='name'
-            variant='outlined'
-            onChange={event => {
-              this.setState({ name: _.startCase(event.target.value) });
-            }}
-            label='Name'
-            disabled={isScoreSaved}
-          />
-          <Button
-            variant='contained'
-            color='secondary'
-            size='large'
-            onClick={this.saveScore}
-            className={styles.saveButton}
-            disabled={isScoreSaved}
-          >
-            {isScoreSaved ? 'Score saved!' : 'Save score'}
-          </Button>
-        </div>
+  return (
+    <div className={styles.saveScore}>
+      <div className={styles.nameWrapper}>
+        <p className={styles.saveText}>Enter your name to save your score:</p>
+        <TextField
+          fullWidth
+          id='name'
+          variant='outlined'
+          onChange={event => {
+            setName(_.startCase(event.target.value));
+          }}
+          label='Name'
+          disabled={isScoreSaved}
+        />
+        <Button
+          variant='contained'
+          color='secondary'
+          size='large'
+          onClick={saveScoreToLocalStorage}
+          className={styles.saveButton}
+          disabled={isScoreSaved}
+        >
+          {isScoreSaved ? 'Score saved!' : 'Save score'}
+        </Button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default SaveScore;
