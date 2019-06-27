018332aed4cf716a884c27ed7d7f940b483d3b62
Fernando Macias <fernandom@motabilityoperations.co.uk>
2019-06-27 09:50:40 +0100

    Simplifies RoundResult and RoundsResult components

--- src/components/RoundsResult/RoundsResult.jsx
+++ src/components/RoundsResult/RoundsResult.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import styles from './RoundsResult.module.scss';
 import RoundResult from '../RoundResult/RoundResult';
 
-const RoundsResult = ({ playedCities }) => {
-  return (
-    <div className={styles.roundsResult}>
-      {playedCities.map(city => {
-        const { real, clicked } = city;
-        return <RoundResult key={city.id} real={real} clicked={clicked} />;
-      })}
-    </div>
-  );
-};
+const RoundsResult = ({ playedCities }) => (
+  <div className={styles.roundsResult}>
+    {playedCities.map(({ real, clicked, id }) => (
+      <RoundResult key={id} real={real} clicked={clicked} />
+    ))}
+  </div>
+);
 
 export default RoundsResult;
