cf623314373a6ca251b88e02ff10627400862f12
Fernando Macias <fernandom@motabilityoperations.co.uk>
2019-06-28 15:03:18 +0100

    Return empty array if no scores available

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,9 +1,7 @@
 export const saveScoreLocally = score => {
-  const currentScores = localStorage.getItem("scores");
-  const newScores = currentScores
-    ? [...JSON.parse(currentScores), score]
-    : [score];
-  localStorage.setItem("scores", JSON.stringify(newScores));
+  const currentScores = localStorage.getItem('scores');
+  const newScores = currentScores ? [...JSON.parse(currentScores), score] : [score];
+  localStorage.setItem('scores', JSON.stringify(newScores));
 };
 
-export const getScores = () => JSON.parse(localStorage.getItem("scores"));
+export const getScores = () => JSON.parse(localStorage.getItem('scores')) || [];
