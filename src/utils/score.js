b358b92809e5db37a6fe8ace505879f4477307fc
Felix Hai Xia <fhai@thoughtworks.com>
2019-07-01 13:48:46 +0100

    Change time component of score to fit a sin wave and thus punishes players less severely in first few seconds of round

--- src/utils/score.js
+++ src/utils/score.js
@@ -6,16 +6,19 @@ const halfEarthCircumference = 40075 / 2;
 const accuracyWeighting = 0.8;
 const timeWeighting = 0.2;
 
-const normalizeScore = score =>
-  Math.pow(score * Math.pow(maxScorePerRound, 1 / Math.E), Math.E);
+export const normaliseAccuracyScore = score =>
+  Math.round(Math.pow(score * Math.pow(maxScorePerRound, 1 / Math.E), Math.E));
+
+export const normaliseTimeScore = score =>
+  Math.round(Math.sin((score * Math.PI) / 2) * 100);
 
 export const calculateTurnScore = (distance, timeLeft) => {
-  if (!distance || !timeLeft) {
+  if ((!distance && distance !== 0) || (!timeLeft && timeLeft !== 0)) {
     return 0;
   }
   const rawDistanceScore = (halfEarthCircumference - distance) / halfEarthCircumference;
-  const distanceScore = normalizeScore(rawDistanceScore);
+  const distanceScore = normaliseAccuracyScore(rawDistanceScore);
   const rawTimeScore = timeLeft / millisecondsPerTurn;
-  const timeScore = normalizeScore(rawTimeScore);
+  const timeScore = normaliseTimeScore(rawTimeScore);
   return Math.round(distanceScore * accuracyWeighting + timeScore * timeWeighting);
 };
--- src/utils/score.test.js
+++ src/utils/score.test.js
@@ -1,18 +1,42 @@
-import { calculateTurnScore } from "./score";
+import { calculateTurnScore, normaliseAccuracyScore, normaliseTimeScore } from './score';
 
-describe("calculateTurnScore", () => {
-  it("should correctly calculate minimum score", () => {
+describe('calculateTurnScore', () => {
+  it('should correctly calculate minimum score', () => {
     const score = calculateTurnScore(20032, 1);
     expect(score).toEqual(0);
   });
 
-  it("should correctly calculate maximum score", () => {
+  it('should correctly calculate maximum score', () => {
     const score = calculateTurnScore(0, 5000);
     expect(score).toEqual(100);
   });
 
-  it("should return correct score according to accuracy and time weightings", () => {
+  it('should return correct score according to accuracy and time weightings', () => {
     const score = calculateTurnScore(0, 1);
     expect(score).toEqual(80);
+  });
+});
+
+describe('normaliseAccuracyScore', () => {
+  it('should get minimum accuracy score', () => {
+    const score = normaliseAccuracyScore(0);
+    expect(score).toEqual(0);
+  });
+
+  it('should get maximum accuracy score', () => {
+    const score = normaliseAccuracyScore(1);
+    expect(score).toEqual(100);
+  });
+});
+
+describe('normaliseTimeScore', () => {
+  it('should get minimum time score', () => {
+    const score = normaliseTimeScore(0);
+    expect(score).toEqual(0);
+  });
+
+  it('should get maximum time score', () => {
+    const score = normaliseTimeScore(1);
+    expect(score).toEqual(100);
   });
 });
